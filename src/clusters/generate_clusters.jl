6916cdc7303bb16affecd9a8b31ee1675ddb43bb
sanderdemeyer <80397440+Sander-De-Meyer@users.noreply.github.com>
2024-12-19 16:49:06 +0100

    resolve conflicts

--- src/clusters/generate_clusters.jl
+++ src/clusters/generate_clusters.jl
@@ -174,6 +174,8 @@ function get_levels(cluster)
     end
 
     lp = longest_path.longest_path
+    println("lp = $(lp)")
+    println(a)
     levels_line = get_levels_line(n)
     levels_dict = Dict()
     for i = 1:n
@@ -336,3 +338,80 @@ function get_conjugated(dir)
         return [Bool[0, 0, 1], Bool[0, 1, 1]]
     end
 end
+
+function solve_cluster(cluster, PEPO, β, twosite_op; loops = true)
+    N = length(cluster)
+    println("in solve cluster - cluster = $(cluster)")
+    exp_H = exponentiate_hamiltonian(twosite_op, cluster, β, length(cluster))
+    residual = contract_PEPS(cluster, PEPO)
+    RHS = exp_H - residual
+    @assert !(any(isnan.(convert(Array,RHS[][:])))) "RHS contains elements that are NaN"
+
+    levels_sites = get_levels_sites(cluster)
+    if N == 4
+        if loops
+            levels_sites = nothing
+            A = solve_4_loop(exp_H; α = 10)
+        else
+            levels_sites = [(0, 1, 0, 0), (0, 0, -2, 1), (-2, 0, 0, 1), (0, 1, 0, 0)]
+        end
+    end
+    if levels_sites === nothing
+        if N == 4.1
+            levels_to_update, solution, err = solve_4_loop(α, RHS)
+            merge!(PEPO, Dict(zip(levels_to_update, solution)))
+        else
+            (levels_sites === nothing) && (return PEPO)
+        end
+    end
+
+    sites_to_update = [i for (i,levels) = enumerate(levels_sites) if !(levels ∈ PEPO.keys)]
+    length(sites_to_update) == 0 && return
+    contraction_indices, conjugated, open_conjugated, open_indices, spaces, number_of_bonds = get_contraction_order(cluster, sites_to_update)
+
+    A = get_A(PEPO, levels_sites, number_of_bonds, sites_to_update, conjugated, open_conjugated, open_indices, contraction_indices)
+
+    if length(sites_to_update) == 2
+        dir = (cluster[sites_to_update[2]][1] - cluster[sites_to_update[1]][1], cluster[sites_to_update[2]][2] - cluster[sites_to_update[1]][2])
+        conjugated = get_conjugated(dir)
+    elseif length(sites_to_update) == 1
+        dir = 0
+        conjugated = [Bool[0, 0, 1, 1]]
+    else
+        error("Something went terribly wrong")
+    end
+    levels_to_update = levels_sites[sites_to_update]
+    solution = solve_index(A, exp_H-residual, conjugated, sites_to_update, levels_to_update, get_direction(dir), length(cluster); spaces = i -> ℂ^(2^(2*i)))
+    merge!(PEPO, Dict(zip(levels_to_update, solution)))
+end
+
+function get_all_indices(PEPO, p, β, twosite_op)
+    for N = 2:2
+        println("N = $(N)")
+        clusters = get_nontrivial_terms(N)
+        for cluster = clusters
+            solve_cluster(cluster, PEPO, β, twosite_op)
+        end
+    end
+    cluster = [(0,0),(1,0),(1,1),(0,1)]
+    solve_cluster(cluster, PEPO, β, twosite_op; loops = false)
+    return PEPO
+end    
+
+function clusterexpansion(p, β, twosite_op, onesite_op)
+    pspace = onesite_op.dom[1]
+    PEPO₀ = init_PEPO(onesite_op)
+    PEPO = get_all_indices(PEPO₀, p, β, twosite_op)
+    return get_PEPO(pspace, PEPO)
+end
+
+# cluster = [(-1, 0), (0, 0), (1, 0)]
+
+# x0 = TensorMap(randn, ℂ^2 ⊗ (ℂ^2)' ← ℂ^(7) ⊗ ℂ^(6) ⊗ (ℂ^(8))' ⊗ (ℂ^(4))')
+# summary(x0)
+
+
+# x0[][:,:,1:4,1,5:8,1] = sol[];
+
+# loop = [(0,0),(0,1),(1,0),(1,1)]
+# exp_H = exponentiate_hamiltonian(loop, β)
---
+++ test/loops_tree.jl
@@ -0,0 +1,22 @@
+using TensorKit
+using MPSKitModels
+using Graphs
+using LongestPaths
+using OptimKit
+import ClusterExpansions: clusterexpansion
+import PEPSKit: rmul!, σᶻᶻ, σˣ
+p = 2
+β = 1
+D = 2
+χenv = 12
+
+J = 1.0
+g = 1.0
+N1, N2 = (1,1)
+
+twosite_op = rmul!(σᶻᶻ(), -1.0)
+onesite_op = rmul!(σˣ(), g * -J)
+
+pspace = ℂ^2
+
+O = clusterexpansion(p, β, twosite_op, onesite_op)

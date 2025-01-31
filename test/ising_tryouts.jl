ff7bfb0c1e48144aac4b604ba2985fdd3c1034d0
sanderdemeyer <sander.demeyer@ugent.be>
2025-01-31 18:29:27 +0100

    include tests

--- test/ising_tryouts.jl
+++ test/ising_tryouts.jl
@@ -6,8 +6,10 @@ using ClusterExpansions
 using OptimKit
 using PEPSKit
 import PEPSKit: rmul!, σᶻᶻ, σˣ, InfiniteSquare, InfinitePartitionFunction
+using Zygote
 
-p = 3
+
+p = 2
 β = 1e-3
 D = 2
 χenv = 8
@@ -21,12 +23,44 @@ N1, N2 = (1,1)
 pspace = ℂ^2
 twosite_op = rmul!(σᶻᶻ(), -1.0)
 onesite_op = rmul!(σˣ(), g * -J)
+# function spaces(i)
+#     if i == -1
+#         return ℂ^(10)
+#     elseif i == 0
+#         return ℂ^(1)
+#     elseif i == 1
+#         return ℂ^(2)
+#     elseif i == 2
+#         return ℂ^(4)
+#     elseif i == 3
+#         return ℂ^(8)
+#     else
+#         return ℂ^(2^(2*i))
+#     end
+# end
 
-spaces = i -> (i >= 0) ? ℂ^(2^(2*i)) : ℂ^10
+spaces = i -> (i >= 0) ? ℂ^(2^(i)) : ℂ^(-10*i)
+# spaces = i -> (i >= 0) ? ℂ^(2^(2*i) - (i == 1)*2) : ℂ^10
 # spaces = i -> (i >= 0) ? ℂ^(1+2*i) : ℂ^(-10*i)
-O_clust = clusterexpansion(p, β, twosite_op, onesite_op; levels_convention = "tree_depth", spaces = spaces, symmetry = symmetry)
+O, O_clust = clusterexpansion(p, β, twosite_op, onesite_op; levels_convention = "tree_depth", spaces = spaces, symmetry = symmetry, verbosity = 1)
 Magn = TensorMap([1.0 0.0; 0.0 -1.0], pspace, pspace)
 PEPO = InfinitePEPO(O_clust)
+
+
+vspace = ℂ^3
+# A = TensorMap(randn, pspace, pspace ⊗ pspace ⊗ pspace' ⊗ pspace')
+# A = flip_arrows(make_translationally_invariant(flip_arrows(A)))
+A = TensorMap(randn, pspace, vspace ⊗ vspace ⊗ vspace' ⊗ vspace')
+A = flip_arrows(make_translationally_invariant(flip_arrows(A)))
+# O_clust = flip_arrows(O_clust)
+# W, error = find_truncation(A, O_clust; step_size = 1e-3, ϵ = 1e-10, max_iter = 10000, line_search = false, linesearch_options = 1, verbosity = 2)
+Ws, A_trunc = find_truncation(A, O_clust)
+println("Relative error = $(norm(A-A_trunc)/norm(A))")
+
+
+A = apply(A, O_clust, Ws; spaces = [vspace], verbosity = 3)
+
+println(a)
 
 @tensor Z[-3 -4; -1 -2] := O_clust[1 1; -1 -2 -3 -4]
 

e58a645b4b0b2bdfb45c793dcca8c7cde1e27a78
sanderdemeyer <sander.demeyer@ugent.be>
2025-09-05 17:49:59 +0200

    update to spaces of J1-J2 model

--- src/models/models.jl
+++ src/models/models.jl
@@ -191,7 +191,54 @@ function spaces_heisenberg(spin_symmetry; loop_space = ℂ^4)
     return spaces, envspace
 end
 
+function spaces_heisenberg_J1_J2(spin_symmetry; loop_space = ℂ^4)
+    if spin_symmetry == Trivial
+        loop_space = ℂ^4
+        spaces = i -> if i == 0
+            ℂ^1
+        elseif i > 0
+            ℂ^(2^(2*i))
+        else
+            loop_space
+        end
+        envspace = χ -> ℂ^χ
+    elseif spin_symmetry == U1Irrep
+        loop_space = Vect[U1Irrep](1 => 2, -1 => 2)
+        spaces = i -> if i == 0
+            Vect[U1Irrep](0 => 1)
+        elseif i > 0
+            Vect[U1Irrep](1 => 2^(2*i-1), -1 => 2^(2*i-1))
+        else
+            loop_space
+        end
+        envspace = χ -> Vect[U1Irrep](0 => div(χ,2), 1 => div(χ,4), -1 => div(χ,4))
+    elseif spin_symmetry == SU2Irrep
+        loop_space = Vect[SU2Irrep](0 => 2, 1 => 2, 2 => 1)
+        spaces = i -> if i == 0
+            Vect[SU2Irrep](0 => 1)
+        elseif i == 1
+            Vect[SU2Irrep](0 => 1, 1 => 1)
+        elseif i == 2
+            Vect[SU2Irrep](0 => 2, 1 => 3, 2 => 1)
+        else
+            loop_space
+        end
+        envspace = χ -> Vect[SU2Irrep](0 => div(χ,2), 1 // 2 => div(χ,4), 1 => div(χ,4))
+    end
+    return spaces, envspace
+end
+
+function heisenberg_XXX_operators(Jx; spin = 1//2, spin_symmetry = Trivial, T = Complex{BigFloat}, loop_space = ℂ^4, kwargs...)
+    twosite_op = rmul!(SpinOperators.S_exchange(T, spin_symmetry; spin=spin), Jx)
+    onesite_op = rmul!(id(SpinOperators.spin_space(spin_symmetry; spin=spin)), T(0))
+    spaces, envspace = spaces_heisenberg(spin_symmetry; loop_space)
+    return ClusterExpansion(twosite_op, onesite_op; spaces, envspace, kwargs...)
+end
+
 function heisenberg_operators(Jx, Jy, Jz, h; spin = 1//2, spin_symmetry = Trivial, T = Complex{BigFloat}, loop_space = ℂ^4, kwargs...)
+    if Jx == Jy == Jz && h == 0.0
+        return heisenberg_XXX_operators(Jx; spin=spin, spin_symmetry=spin_symmetry, T=T, loop_space=loop_space, kwargs...)
+    end
     if spin_symmetry == Trivial
         twosite_op =  rmul!(SpinOperators.S_x_S_x(T, spin_symmetry; spin=spin), Jx) +
                 rmul!(SpinOperators.S_y_S_y(T, spin_symmetry; spin=spin), Jy) +
@@ -213,9 +260,14 @@ end
 function J1J2_operators(J1, J2, h; spin = 1//2, spin_symmetry = Trivial, T = Complex{BigFloat}, loop_space = ℂ^4, kwargs...)
     twosite_op =  rmul!(SpinOperators.S_exchange(T, spin_symmetry; spin), J1)
     nn_term = rmul!(SpinOperators.S_exchange(T, spin_symmetry; spin), J2)
-    onesite_op = rmul!(SpinOperators.S_z(T, spin_symmetry; spin), h)
+    if h == 0.0
+        pspace = domain(twosite_op)[1]
+        onesite_op = rmul!(id(pspace), 0.0)
+    else
+        onesite_op = rmul!(SpinOperators.S_z(T, spin_symmetry; spin), h)
+    end
 
-    spaces, envspace = spaces_heisenberg(spin_symmetry; loop_space)
+    spaces, envspace = spaces_heisenberg_J1_J2(spin_symmetry; loop_space)
     return ClusterExpansion(twosite_op, onesite_op; nn_term, spaces, envspace, kwargs...)
 end
 
